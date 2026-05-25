c28485b508d55cdf54d8c8e699fe2db67666ebd0
RamanandSabnis-tipic <ramanand.sabnis@tipic.in>
2026-05-25 18:43:54 +0530

    chore: configure database session driver and disable stateful middleware in Sanctum to fix idle timeout

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->api(prepend: [
-            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
         ]);
 
         $middleware->alias([
